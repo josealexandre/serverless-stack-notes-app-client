9e596298937e6b30edf15f0d192c5d4fbb818678
Alexandre Junior <jasj_jr@hotmail.com>
2019-06-10 17:39:43 -0300

    Configuring AWS Amplify to connect to the backend resources

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: 'us-east-2',
+        BUCKET: 'alexandre-notes-app-uploads'
+    },
+    apiGateway: {
+        REGION: 'us-east-2',
+        URL: 'https://ybich7oyx0.execute-api.us-east-2.amazonaws.com/prod'
+    },
+    cognito: {
+        REGION: 'us-east-2',
+        USER_POOL_ID: 'us-east-2_q2byaoDyq',
+        APP_CLIENT_ID: '62n5ckhs9m8vol0qa7jq7pk4hl',
+        IDENTITY_POOL_ID: 'us-east-2:2937b798-e58d-4d80-8a68-739923b16864'
+    }
+}
